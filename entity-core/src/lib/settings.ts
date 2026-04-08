042d900a1770594351440cb3ac2429134062a9ea
Paul Mikol <paul.mikol@gmail.com>
2026-04-08 02:48:20 -0500

    m

--- entity-core/src/lib/settings.ts
+++ entity-core/src/lib/settings.ts
@@ -12,20 +12,19 @@ export const settings = {
     process.env.NEXT_PUBLIC_ENTITY_CORE_API_BASE_URL ||
      "https://entity-core.fullstackjedi.dev/api",
 
-  AUTH0_DOMAIN: required(
-    "NEXT_PUBLIC_AUTH0_DOMAIN",
-    process.env.NEXT_PUBLIC_AUTH0_DOMAIN
-  ),
+  AUTH0_DOMAIN:
+   process.env.NEXT_PUBLIC_AUTH0_DOMAIN ||
+     "",
 
-  AUTH0_CLIENT_ID: required(
-    "NEXT_PUBLIC_AUTH0_CLIENT_ID",
-    process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID
-  ),
 
-  AUTH0_AUDIENCE: required(
-    "NEXT_PUBLIC_AUTH0_AUDIENCE",
-    process.env.NEXT_PUBLIC_AUTH0_AUDIENCE
-  ),
+  AUTH0_CLIENT_ID:
+    process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID ||
+    "",
+
+  AUTH0_AUDIENCE:
+
+    process.env.NEXT_PUBLIC_AUTH0_AUDIENCE ||
+    "https://entity-core.fullstackjedi.dev/api",
 
   AUTH0_NAMESPACE:
     process.env.NEXT_PUBLIC_AUTH0_NAMESPACE ||
