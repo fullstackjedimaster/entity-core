0e3909274ae86bff8d61218df55c67075c21e29a
Paul Mikol <paul.mikol@gmail.com>
2026-04-21 22:52:31 -0500

    auto: sync from droplet

--- entity-core/src/components/EntityEditor.tsx
+++ entity-core/src/components/EntityEditor.tsx
@@ -12,6 +12,7 @@ export default function EntityEditor({ entityName: initialName }: { entityName?:
 
     // Load when entityName changes
     useEffect(() => {
+        debugger;
         if (entityName) {
             loadEntity();
         }
