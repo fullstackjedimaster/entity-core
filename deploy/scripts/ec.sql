8fb676f2d11bf0b240a185acab015173d404fe73
Paul Mikol <paul.mikol@gmail.com>
2026-04-28 14:58:31 -0500

    auto: sync from droplet

--- deploy/scripts/ec.sql
+++ deploy/scripts/ec.sql
@@ -139,7 +139,7 @@ DECLARE
   update_pairs text := '';
   query text;
   zero_uuid constant uuid := '00000000-0000-0000-0000-000000000000'::uuid;
-  target_schema text :=lower(coalesce(schema,''));;
+  target_schema text :=lower(coalesce(schema,''));
   table_name text :=  lower(coalesce(entity,''));
   arg_list text := '';
     arg text;
